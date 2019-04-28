9c748310e40acf31bc13babb0f218c1049defce1
lpf260 <754476346@qq.com>
2019-04-28 21:08:48 +0800

    TheHeader

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,28 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>Welcome to VuejsCaff.com</h1>
+  <div id="wrap">
+    <TheHeader/>
+    <!--在单文件组件中，没有内容的组件应该是闭合的，所以不要忘记使用斜杠/进行闭合-->
   </div>
 </template>
 
 <script>
+import TheHeader from "@/components/layouts/TheHeader";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    TheHeader
+  }
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 200px;
-}
+$icon-font-path: "~bootstrap-sass/assets/fonts/bootstrap/"; /*$在sass语法中表示变量，用于指定自提路径*/
+$fa-font-path: "~font-awesome/fonts/";
+@import "~bootstrap-sass/assets/stylesheets/_bootstrap";
+@import "~font-awesome/scss/font-awesome";
 
-h1{
-  color: #42b983;
+body > .container {
+  margin-top: 15px;
 }
 </style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="navbar navbar-default topnav">
+    <div class="container">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="/">
+          <span class="title">{{logo.title}}</span>
+          <img :src="logo.src" :alt="logo.title">
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheHeader", // 组件的名称
+  data() {
+    return {
+      logo: {
+        src: `${this.uploadsUrl}sites/ByvFbNlQYVwhvTyBgLdqitchoacDNznN.jpg`,
+        title: "VuejsCaff"
+      }
+    };
+  },
+  beforeCreate() {
+    // 生命周期钩子的一部分，在实例初始化之后，数据观测之前被调用
+    //在当前实例上添加一个uploadsUrl属性
+    this.uploadsUrl = "https://vuejscaffcdn.phphub.org/uploads/";
+  }
+};
+</script >
+
+<style scoped>
+.title {
+  display: none;
+}
+</style>
